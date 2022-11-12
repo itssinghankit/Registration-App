ffb23f647a4f25bfb881959f631f5e60d5fc7394
itssinghnakit <singhankit.kr@com.gmail>
2022-11-12 20:09:43 +0530

    otp page

--- app/src/main/java/com/example/registrationapp/signup.kt
+++ app/src/main/java/com/example/registrationapp/signup.kt
@@ -1,8 +1,10 @@
 package com.example.registrationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
 class signup : AppCompatActivity() {
@@ -11,9 +13,16 @@ class signup : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         val branch=findViewById<Spinner>(R.id.branch)
+        val signup=findViewById<Button>(R.id.signupNext)
+
         ArrayAdapter.createFromResource(this,R.array.branch,android.R.layout.simple_spinner_item).also{
             adapter->adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             branch.adapter=adapter
+        }
+
+        signup.setOnClickListener{
+            val intent=Intent(this,otpverification::class.java)
+            startActivity(intent)
         }
 
     }
